4bc2bbf4e2a9fee1900c031a9949c3a11a6a0853
Nicolas Housset <nicolas.housset@gmail.com>
2022-09-06 12:50:12 +0200

    Update batch-all-cas-table-in-memory.sh

--- batch-all-cas-table-in-memory.sh
+++ batch-all-cas-table-in-memory.sh
@@ -1,4 +1,4 @@
 /opt/sas/spre/home/SASFoundation/sas  -cashost $1 -casport $2 -casauthinfo  $3 sas/batch-all-cas-table-in-memory.sas -log /tmp/batch-all-cas-table-in-memory.log
 
 
-grep ANALYSE_MEMOIRE batch-all-cas-table-in-memory.log |grep -v "||"
+grep ANALYSE_MEMOIRE /tmp/batch-all-cas-table-in-memory.log |grep -v "||"
