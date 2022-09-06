aebcc58f974baab808efd078e8aa59858eb86936
Nicolas Housset <nicolas.housset@gmail.com>
2022-09-06 13:16:46 +0200

    Update and rename allCasTableInMemory.sh to CASsTableInMemory.sh

--- CASsTableInMemory.sh
+++ CASsTableInMemory.sh
@@ -1,4 +1,5 @@
 /opt/sas/spre/home/SASFoundation/sas  -cashost $1 -casport $2 -casauthinfo  $3 sas/batch-all-cas-table-in-memory.sas -log /tmp/batch-all-cas-table-in-memory.log
 
-
+cat /tmp/batch-all-cas-table-in-memory.log
+echo "============================================="
 grep ANALYSE_MEMOIRE /tmp/batch-all-cas-table-in-memory.log |grep -v "||"
